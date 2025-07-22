24b031ab204b79363a1482e271abb04214750080
Chris <64163980+thenextbutton@users.noreply.github.com>
2025-07-23 00:00:08 +0100

    Update options_flow.py

--- custom_components/jellyfin_status/options_flow.py
+++ custom_components/jellyfin_status/options_flow.py
@@ -6,8 +6,9 @@ from .const import DOMAIN
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
-        self.config_entry = config_entry
+        super().__init__(config_entry)
         self._translations = {}
+
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
